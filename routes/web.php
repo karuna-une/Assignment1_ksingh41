e090186e75f7a60dff8fd3e670d4be5e2a9fc58b
Karuna Singh <ksingh41@myune.edu.au>
2024-07-21 13:46:13 +1000

    initial commit! added create post and view post functionality

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\PhotoController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// Route::get('/posts', [PostController::class,'index'])->name('posts.index');
+// Route::get('/posts/create', [PostController::class,'create'])->name('posts.create');
+// Route::get('/posts/show', [PostController::class,'show'])->name('posts.show');
+
+
+Route::resource('posts',PostController::class);
+Route::resource('photos',PhotoController::class);
+
