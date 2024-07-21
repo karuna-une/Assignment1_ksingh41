ce2b2c2eba57928c546e46b5f78493067b4fe7dd
Karuna Singh <ksingh41@myune.edu.au>
2024-07-21 15:44:35 +1000

    laravel UI added successfully

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,6 +14,7 @@ class PostController extends Controller
      */
     public function index()
     {
+        dd(Auth::user());
         $posts = Post::all();
         return view('Posts.index', compact('posts'));
     }
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,15 @@ Route::get('/', function () {
 // Route::get('/posts/show', [PostController::class,'show'])->name('posts.show');
 
 
-Route::resource('posts',PostController::class);
-Route::resource('photos',PhotoController::class);
+Route::group(['middleware' => ['auth']],function(){
+    Route::resource('posts',PostController::class);
+    Route::resource('photos',PhotoController::class);
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
+
+
+
+
+Auth::routes();
+
 
