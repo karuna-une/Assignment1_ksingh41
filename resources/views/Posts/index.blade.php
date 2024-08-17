a3998d8a29a60257933f2a0ab750754cc93087ff
Karuna Singh <ksingh41@myune.edu.au>
2024-08-18 09:08:02 +1000

    dashboard and layout created successfully

--- resources/views/Posts/index.blade.php
+++ resources/views/Posts/index.blade.php
@@ -1,6 +1,18 @@
-@extends('layouts.app')
+@extends('layouts.admin_layout')
 
 @section('content')
+
+
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 class="h2">Blog Posts</h1>
+            <div class="btn-toolbar mb-2 mb-md-0">
+            <div class="btn-group me-2">
+                <a href="{{ route('posts.create') }}" class="btn btn-sm btn-outline-secondary">Create Post</a>
+            </div>
+            </div>
+        </div>
+
+
     <style>
         /* Add custom styles for the table and action buttons here */
         table {
@@ -20,11 +32,7 @@
     </style>
 
     <div class="container">
-        <!-- Heading for the blog posts list -->
-        <h1>Blog Posts</h1>
-        
-        <!-- Link to create a new post -->
-        <a href="{{ route('posts.create') }}" class="btn btn-primary">Create Post</a>
+
         
         <!-- Table displaying the list of blog posts -->
         <table>
