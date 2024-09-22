fd2d27f5d9e6c88cd13559b1d92ee20a0fe5a085
Karuna Singh <ksingh41@myune.edu.au>
2024-09-22 19:59:34 +1000

    API and sanctum added successfully

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            'email' => ['required','email',',exists:users'],
+            'password' => ['required','min:6'],
+        ]);
+
+        $user = User::where('email', $data['email'])->first();
+
+        if(!$user || !Hash::check($data['password'], $user->password))
+        {
+            return response()->json([
+                'data' => null,
+                'message' => 'Wrong Credentislas',
+
+            ],401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ]);
+
+    }
+}
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,11 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use MongoDB\Laravel\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Belongsto;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
 // use Illuminate\Support\Facades\Auth;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens, Notifiable;
 
     protected $fillable = ['title', 'content','user_id'];
 
